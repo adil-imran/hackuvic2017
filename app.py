df498dc460f0873082dc020513ce200279c48934
Rodney Gelera <rodneyg94@gmail.com>
2017-03-11 21:37:36 -0800

    removed commented out code

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template
 import newspaper
-#from apscheduler.schedulers.background import BackgroundScheduler
-#from apscheduler.triggers.interval import IntervalTrigger
-#import datetime
-#import atexit
 
 app = Flask(__name__)
-
-"""def get_news_articles():
-    print("Fetching new articles...")
-    return ["first {0}".format(datetime.datetime.now()), 
-            "second {0}".format(datetime.datetime.now()), 
-            "third {0}".format(datetime.datetime.now())]"""
 
 def get_news_articles(url):
 	paper = newspaper.build(url, memoize_articles=False, keep_article_html=True) # caching off
@@ -25,18 +15,6 @@ def get_news_articles(url):
 		articles.append([article.url, authors, article.title, article.text, article.top_image])
 
 	return articles
-
-"""@app.before_first_request
-def initialize():
-    scheduler = BackgroundScheduler()
-    scheduler.start()
-    scheduler.add_job(
-        func = get_news_articles,
-        trigger=IntervalTrigger(seconds=5),
-        id='get_news_articles_job',
-        name='Get news articles every 3600 seconds',
-        replace_existing=True)
-    atexit.register(lambda: scheduler.shutdown())"""
 
 @app.route("/")
 def hello():
